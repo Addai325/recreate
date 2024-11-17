b3b835e474574e7c311c1d761e62b55f9a98e8b5
Ransford <ransfordaddai111@gmail.com>
2024-11-17 02:48:26 -0800

    checking db with example mistake

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ app = Flask(__name__)
 # Configuration
 app.config['SECRET_KEY'] = 'mysecretkey'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Addai325:Extra111??!!@Addai325.mysql.pythonanywhere-services.com/Addai325$default'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'amysql+pymysql://Addai325:Extra111??!!@Addai325.mysql.pythonanywhere-services.com/Addai325$omniblogs'
 
 
 # Initialize extensions
